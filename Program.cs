48b1c257884e3e400786bd7de3ebb5af5f964cf9
PAPPU KUMAR KESHARI <92083274+Kesharipappu@users.noreply.github.com>
2022-03-12 21:30:36 -0800

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Calculator;
+namespace UsingAssembly
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            MyCalc calc = new MyCalc();
+            Console.WriteLine(calc.Add(700,300));
+            Console.WriteLine(calc.Multiply(15,30));
+        }
+    }
+}
